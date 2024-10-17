2fc3727761376faff74aeace71f91f034e25e003
Chinosato <32897424+Chinosato@users.noreply.github.com>
2024-10-17 19:08:41 +0700

    เพิ่มส่วน_navbar
    git commit -mเพิ่มส่วน_navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <>
+    
       <h1 className="text-4xl font-mono text-orange-300 flex flex-row justify-center g-4 p-4">
         Init Skooldio Project Website !
       </h1>
