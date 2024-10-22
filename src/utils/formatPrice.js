fb4e4bb05982548f62ed50aa2606929b0d8f0f31
PeamMiiZ <peemmygg@gmail.com>
2024-10-22 22:33:20 +0700

    fix style of product card following to UI Design

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,9 @@
+const formatPrice = (price) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+};
+
+export default formatPrice;
