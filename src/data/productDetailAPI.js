57fb5994971b736bb361ac53ccdfaeb761e2df9f
PeamMiiZ <peemmygg@gmail.com>
2024-10-23 15:26:49 +0700

    init feature ProductDetail Card with Call Test API successfully

---
+++ src/data/productDetailAPI.js
@@ -0,0 +1,3 @@
+// https://api.storefront.wdb.skooldio.dev/products/:permalink
+const TestProductDetailAPI = `https://api.storefront.wdb.skooldio.dev/products/accessories-abstratct-printed-scarf`;
+export default TestProductDetailAPI;
