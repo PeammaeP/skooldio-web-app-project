50af071e801309b01d6ce363416dec092a0ac10d
PeamMiiZ <peemmygg@gmail.com>
2024-10-29 00:46:52 +0700

    resolved responsive design in full-scale and change-scale in first page

--- src/components/appProductCard/FeaturedProductCard.jsx
+++ src/components/appProductCard/FeaturedProductCard.jsx
@@ -19,21 +19,21 @@ const FeaturedProductCard = ({
   }, [promotionalPrice, price]);
 
   return (
-    <div className="w-[267px] h-[425px] overflow-hidden justify-start bg-white hover:-translate-y-2">
+    <div className="mt-6 dtdf:w-[370px] dtdf:h-[524px] dtcs:w-[267px] dtcs:h-[425px] overflow-hidden justify-start bg-white hover:-translate-y-2">
       <div className="relative">
         <img
-          className="w-[267px] h-[267px] object-cover"
+          className="dtdf:w-[370px] dtdf:h-[370px] dtcs:w-[267px] dtcs:h-[267px] object-cover"
           src={imageUrls}
           alt={name}
         />
         {discounted !== 100 && discounted !== 0 ? (
-          <div className="absolute w-[66px] h-[34px] top-[24px] right-0 bg-[#FF000D] text-white text-center gap-2 pt-[7px] px-[10px] pb-[7px] text-sm">
+          <div className="absolute w-[66px] h-[34px] top-[24px] right-0 bg-[#FF000D] text-white break-all text-center gap-2 pt-[7px] px-[10px] pb-[7px] text-sm">
             - {discounted}%{" "}
           </div>
         ) : null}
       </div>
       <div className="mt-4">
-        <div className="font-bold text-xl mb-2 line-clamp-1">{name}</div>
+        <div className="font-bold text-xl mb-2 line-clamp-1 ">{name}</div>
         <p
           className=" 
           text-muted-foreground
@@ -56,7 +56,7 @@ const FeaturedProductCard = ({
             />
           ))}
         </div>
-        <div className="flex flex-col mt-4 items-end">
+        <div className="flex flex-col mt-3 items-end">
           {promotionalPrice && promotionalPrice < price ? (
             <div className="flex flex-row items-center gap-4">
               <span className="text-gray-500 line-through mr-2">
